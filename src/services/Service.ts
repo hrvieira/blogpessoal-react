277f0a37f5fd63485a51fd9bbcdfd88fc3b0d700
hrvieira <luizhrvieira@outlook.com>
2024-05-29 12:47:07 -0300

    fix: corrindo api em service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
+export const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
      const resposta = await api.post(url, dados);
